7d09e9bd6ce5c557fdda5c351b4c1dd0c63f3815
Yevhen Tereshchenko <tereschenko.eugene@gmail.com>
2020-04-25 22:33:37 +0300

    fix

--- Hometask_4_8/Scripts/home_directory_date_and_name_file.sh
+++ Hometask_4_8/Scripts/home_directory_date_and_name_file.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-
 ls -R -la $HOME | awk '{print $7 $6"\t"$8"\t"$9}' > home_directory_date_and_name_file.txt
--- Hometask_4_8/Scripts/script6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ls | sed -n '/.*\./p' > search_file_by_it_exception.txt 
