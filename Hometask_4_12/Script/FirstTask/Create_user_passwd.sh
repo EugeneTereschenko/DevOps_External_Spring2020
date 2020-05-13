1933b8bc078fa9ee5044c2abb932368254daae0f
Yevhen Tereshchenko <tereschenko.eugene@gmail.com>
2020-05-13 15:37:35 +0300

    Fix

--- Hometask_4_12/Script/FirstTask/Create_user_passwd.sh
+++ Hometask_4_12/Script/FirstTask/Create_user_passwd.sh
@@ -2,6 +2,11 @@
 
 while read line;
 do var1=$(echo $line | cut -f2 -d' ')
+var2=$(openssl rand -base64 8)
+
+sudo useradd -m $var1 -p $var2
+echo $var1:$var2 | sudo chpasswd
+
 touch $var1-login-password.txt
-openssl rand -base64 8 > $var1-login-password.txt 
+echo $var2 > $var1-login-password.txt 
 done < users.txt
